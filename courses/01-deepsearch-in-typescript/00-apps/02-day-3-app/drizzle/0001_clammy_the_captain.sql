4e0078ac522ff43117ffcfff19d2694bf82b1b2f
Jack Priestman <jack.priestman@gmail.com>
2025-07-16 14:04:51 +0100

    feat: add langfuse

---
+++ courses/01-deepsearch-in-typescript/00-apps/02-day-3-app/drizzle/0001_clammy_the_captain.sql
@@ -0,0 +1,8 @@
+DROP TABLE "ai-app-template_request";--> statement-breakpoint
+DROP INDEX IF EXISTS "chat_user_id_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "message_chat_id_idx";--> statement-breakpoint
+ALTER TABLE "ai-app-template_chat" ALTER COLUMN "updated_at" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "ai-app-template_message" ALTER COLUMN "order" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "ai-app-template_message" ALTER COLUMN "parts" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "ai-app-template_message" ALTER COLUMN "role" SET DATA TYPE varchar(255);--> statement-breakpoint
+ALTER TABLE "ai-app-template_message" DROP COLUMN IF EXISTS "content";
